6df05ac8033708d8ce12ccac162eae271806dbdc
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2021-12-20 13:46:18 -0800

    Day 01, second problem

--- 01/first.py
+++ 01/first.py
@@ -1,10 +1,12 @@
 #! python
 
-from more_itertools import pairwise
+from more_itertools import pairwise, windowed
 
 # Read input from file
 data = [int(x) for x in open('01/input.txt', 'r').readlines()]
 
 # Compute increases
-nincreases = len([(x0, x1) for x0, x1 in pairwise(data) if x1 > x0])
+sliding_sums = [sum(xs) for xs in windowed(data, 3)]
+nincreases = len([(x0, x1) for x0, x1 in pairwise(sliding_sums) if x1 > x0])
+
 print(nincreases)
