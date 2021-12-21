a4a25d91df6ff064f41116af3b8ec1ddccd0741f
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2021-12-21 14:16:14 -0800

    day 03, second problem

--- 03/main.py
+++ 03/main.py
@@ -35,9 +35,48 @@ def bits_to_number(bits):
   return number
 
 
+def search_for_rating(data, most_common_value_fn):
+  nbits = data.shape[1]
+  subrange = data
+
+  for i in range(nbits):
+    col = subrange[:, i]
+    ones = np.sum(np.asarray((col == 1), dtype=int))
+    zeros = np.sum(np.asarray((col == 0), dtype=int))
+    most_common_value = most_common_value_fn(zeros, ones)
+    subrange = subrange[subrange[:, i] == most_common_value]
+
+    assert (subrange.shape[0] >= 1)
+
+    if subrange.shape[0] == 1:
+      break
+
+  return subrange[0]
+
+
+def find_oxygen_generator_rating(data):
+  def most_common_value(zeros, ones):
+    return 1 if ones >= zeros else 0
+
+  return bits_to_number(search_for_rating(data, most_common_value))
+
+
+def find_co2_scrubber_rating(data):
+  def most_common_value(zeros, ones):
+    return 0 if zeros <= ones else 1
+
+  return bits_to_number(search_for_rating(data, most_common_value))
+
+
 most_common_bits = get_most_common_bits(data)
 gamma_rate = bits_to_number(most_common_bits)
 epsilon_rate = bits_to_number(flip_bits(most_common_bits))
 
+oxygen_generator_rating = find_oxygen_generator_rating(data)
+co2_scrubber_rating = find_co2_scrubber_rating(data)
+
 print(f"gamma rate: {gamma_rate}")
 print(f"epsilon rate: {epsilon_rate}")
+
+print(f"oxygen generator rating: {oxygen_generator_rating}")
+print(f"co2 scrubber rating: {co2_scrubber_rating}")
