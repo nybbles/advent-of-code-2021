2f28e8714d73b5def551a95e67f32c2945c46dcf
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2021-12-22 11:21:23 -0800

    day 06, second problem

--- 06/main.py
+++ 06/main.py
@@ -1,27 +1,38 @@
 #! python
 
-from itertools import chain
-from more_itertools import ilen
+from more_itertools import ilen, bucket
+from collections import defaultdict
 
-school = [3, 4, 3, 1, 2]
+# school = [3, 4, 3, 1, 2]
 school = [int(x.strip()) for x in open("06/input.txt", 'r').read().split(",")]
 
 
-def simulate_one_day_one_fish(fish):
-  if fish > 0:
-    return [fish - 1]
-  else:
-    return [6, 8]
+def school_histogram(school):
+  bucketed = bucket(school, key=lambda x: x)
+  return {x: ilen(bucketed[x]) for x in list(bucketed)}
 
 
-def simulate_one_day(school):
-  return chain(*[simulate_one_day_one_fish(fish) for fish in school])
+def simulate_one_day(school_histogram):
+  next_day_histogram = defaultdict(int)
+  for age, number in school_histogram.items():
+    if age > 0:
+      next_day_histogram[age - 1] = next_day_histogram[age - 1] + number
+    else:
+      next_day_histogram[8] = next_day_histogram[8] + number
+      next_day_histogram[6] = next_day_histogram[6] + number
+
+  return next_day_histogram
 
 
-def simulate_days(school, ndays):
-  for _ in range(ndays):
-    school = simulate_one_day(school)
-  return school
+def simulate_days(school_histogram, ndays):
+  for i in range(ndays):
+    print(f"simulating day {i}")
+    school_histogram = simulate_one_day(school_histogram)
+  return school_histogram
 
 
-print(ilen(simulate_days(school, 80)))
+def count_fish_in_histogram(school_histogram):
+  return sum(school_histogram.values())
+
+
+print(count_fish_in_histogram(simulate_days(school_histogram(school), 256)))
