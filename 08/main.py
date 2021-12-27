bd7dd12b9511e541c54e8703303bee58b5e37c53
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2021-12-27 12:57:36 -0800

    Day 08, second problem (#2)

--- 08/main.py
+++ 08/main.py
@@ -1,6 +1,6 @@
 #! python
 
-from itertools import chain
+from more_itertools import bucket
 
 input = """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
@@ -29,18 +29,104 @@ input = [(set(parse_signal_patterns(unique_signal_patterns)),
 # segments, "4" uses 4 segments, "8" uses all 7 segments and "7" uses 3
 unique_segment_lengths = set([2, 4, 7, 3])
 
-output_values = list(zip(*input))[1]
+segment_length_to_digits = {
+    # Unique segment lengths
+    2: set([1]),
+    4: set([4]),
+    7: set([8]),
+    3: set([7]),
+
+    # Non-unique segment lengths
+    6: set([0, 6, 9]),
+    5: set([2, 3, 5]),
+}
 
 
-def flatten_one_level(xss):
-  for xs in xss:
-    for x in xs:
-      yield x
+def patterns_by_segment_length(segment_patterns):
+  bucketed = bucket(segment_patterns, key=lambda x: len(x))
+  return {length: set(bucketed[length]) for length in list(bucketed)}
 
 
-ans = [
-    output_value for output_value in flatten_one_level(output_values)
-    if len(output_value) in unique_segment_lengths
-]
+def segment_to_digit_mapping(by_segment_lengths):
+  mapping = {
+      digit: set(list(patterns)[0])
+      for digit, patterns in {
+          1: by_segment_lengths[2],
+          4: by_segment_lengths[4],
+          7: by_segment_lengths[3],
+          8: by_segment_lengths[7],
+      }.items()
+  }
 
-print(len(ans))
+  # Number "1" only uses right-side segments.
+  # Number "4" is "1" plus middle segment and top-left segment.
+  # Number "7" is "1" plus top-middle segment.
+
+  # Disambiguation for six segments:
+  # Numbers "4" plus "7" is a strict subset of number "9".
+  union_4_7 = mapping[4].union(mapping[7])
+  mapping[9] = set([
+      segment for segment in by_segment_lengths[6]
+      if set(segment).issuperset(union_4_7)
+  ][0])
+
+  # Number "6" has exactly one of the segments in "1" missing.
+  mapping[6] = set([
+      segment for segment in by_segment_lengths[6]
+      if len(mapping[1].difference(set(segment))) == 1
+  ][0])
+
+  # Therefore, "0"  is the remaining six segment digit.
+  mapping[0] = set([
+      segment for segment in by_segment_lengths[6]
+      if not (set(segment).issuperset(union_4_7)
+              or len(mapping[1].difference(set(segment))) == 1)
+  ][0])
+
+  # Disambiguation for five segments
+  # Number "5" has one segment removed from "6"
+  mapping[5] = set([
+      segment for segment in by_segment_lengths[5]
+      if set(mapping[6]).issuperset(segment)
+  ][0])
+
+  # Number "3" is a super-set of "1", and "2" and "5" are not.
+  mapping[3] = set([
+      segment for segment in by_segment_lengths[5]
+      if set(segment).issuperset(mapping[1])
+  ][0])
+
+  # Therefore, "2" is the remaining five segment digit.
+  mapping[2] = set([
+      segment for segment in by_segment_lengths[5]
+      if not (set(mapping[6]).issuperset(segment)
+              or set(segment).issuperset(mapping[1]))
+  ][0])
+
+  # invert mapping to get segment to digit
+  inverted_mapping = {
+      "".join(sorted(list(value))): key
+      for key, value in mapping.items()
+  }
+
+  return inverted_mapping
+
+
+def decode_number(segment_to_digit, segments):
+  digits = [
+      segment_to_digit[segment] for segment in
+      ["".join(sorted(list(segments))) for segments in segments]
+  ]
+  number = int("".join([str(digit) for digit in digits]))
+  return number
+
+
+def decode_all_numbers(input):
+  for signal_patterns, segments in input:
+    segment_to_digit = segment_to_digit_mapping(
+        patterns_by_segment_length(signal_patterns))
+    yield decode_number(segment_to_digit, segments)
+
+
+ans = sum(decode_all_numbers(input))
+print(ans)
