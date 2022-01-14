26ae4a17bff5ec3ec5fe85eacb6e61bac453be32
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2022-01-13 20:02:34 -0800

    stuck on some lifetime crap

--- d18/src/sliding_window.rs
+++ d18/src/sliding_window.rs
@@ -1,27 +1,61 @@
+use std::collections::VecDeque;
+
 const WINDOW_SIZE: usize = 3;
+
+type OptionalIteratorItem<I: Iterator> = Option<I::Item>;
+type SlidingWindowItem<'a, I: Iterator> = (
+  &'a OptionalIteratorItem<I>,
+  &'a OptionalIteratorItem<I>,
+  &'a OptionalIteratorItem<I>,
+);
+
+type SlidingWindowBuffer<I: Iterator> = VecDeque<OptionalIteratorItem<I>>;
+
+enum SlidingWindowState {
+  BufferToBeFilled,
+  BufferFilled,
+  BufferEmptied,
+}
 
 struct SlidingWindow<I: Iterator> {
   iter: I,
-  buffer: [Option<I::Item>; WINDOW_SIZE],
+  buffer: SlidingWindowBuffer<I>,
+  state: SlidingWindowState,
+}
+
+fn buffer_to_sliding_window_item<I: Iterator>(
+  buffer: SlidingWindowBuffer<I>,
+) -> SlidingWindowItem<I> {
+  assert_eq!(buffer.len(), WINDOW_SIZE);
+  (&buffer[0], &buffer[1], &buffer[2])
 }
 
 impl<I> Iterator for SlidingWindow<I>
 where
   I: Iterator,
 {
-  type Item = [Option<I::Item>; WINDOW_SIZE];
+  type Item = SlidingWindowItem<I>;
 
-  fn next(&mut self) -> Option<[Option<I::Item>; WINDOW_SIZE]> {
-    None
+  fn next(&mut self) -> Option<SlidingWindowItem<I>> {
+    match (self.iter.next(), self.state) {
+      (None, SlidingWindowState::BufferEmptied) => None,
+      (None, SlidingWindowState::BufferFilled) => {
+        self.buffer.pop_front();
+        self.buffer.push_back(None);
+        let item: SlidingWindowItem<I> = buffer_to_sliding_window_item::<I>(self.buffer);
+        Some(item)
+      }
+    }
   }
 }
 
 impl<I: Iterator> SlidingWindow<I> {
   fn new(iter: I) -> SlidingWindow<I> {
-    let buffer: [Option<I::Item>; WINDOW_SIZE] = Default::default();
+    let buffer = VecDeque::from([None, None, None]);
     SlidingWindow {
       iter: iter,
       buffer: buffer,
+      state: SlidingWindowState::BufferToBeFilled,
     }
   }
 }
@@ -29,8 +63,7 @@ impl<I: Iterator> SlidingWindow<I> {
 #[test]
 fn test_sliding_window() {
   let input = vec![1u32, 2, 3];
-  let result: Vec<[Option<u32>; WINDOW_SIZE]> =
-    SlidingWindow::new(input.iter().map(|&x| x)).collect();
+  let result: Vec<Vec<Option<u32>>> = SlidingWindow::new(input.iter().map(|&x| x)).collect();
   let expected = vec![
     [None, Some(1), Some(2)],
     [Some(1), Some(2), Some(3)],
