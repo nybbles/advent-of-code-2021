8b1f95f4a4dc5fa9f97cfc5b9d434658380afb9f
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2022-01-14 15:57:27 -0800

    Using clone now, instead of references/borrowing

--- d18/src/sliding_window.rs
+++ d18/src/sliding_window.rs
@@ -1,16 +1,17 @@
 use std::collections::VecDeque;
+use std::rc::Rc;
 
 const WINDOW_SIZE: usize = 3;
 
-type OptionalIteratorItem<'a, T> = Option<&'a T>;
+type OptionalIteratorItem<T> = Option<T>;
 
-type SlidingWindowItem<'a, T> = (
-  OptionalIteratorItem<'a, T>,
-  OptionalIteratorItem<'a, T>,
-  OptionalIteratorItem<'a, T>,
+type SlidingWindowItem<T> = (
+  OptionalIteratorItem<T>,
+  OptionalIteratorItem<T>,
+  OptionalIteratorItem<T>,
 );
 
-type SlidingWindowBuffer<'a, T> = VecDeque<OptionalIteratorItem<'a, T>>;
+type SlidingWindowBuffer<T> = VecDeque<OptionalIteratorItem<T>>;
 
 enum SlidingWindowState {
   IterationNotStarted,
@@ -18,46 +19,63 @@ enum SlidingWindowState {
   IterationDone,
 }
 
-struct SlidingWindow<'a, I: Iterator> {
+struct SlidingWindow<I>
+where
+  I: Iterator,
+  I::Item: Clone,
+{
   iter: I,
-  buffer: SlidingWindowBuffer<'a, I::Item>,
+  buffer: SlidingWindowBuffer<I::Item>,
   state: SlidingWindowState,
 }
 
-fn buffer_to_sliding_window_item<'a, 'b, I: Iterator>(
-  buffer: &'a SlidingWindowBuffer<'b, I::Item>,
-) -> SlidingWindowItem<'b, I::Item> {
-  assert_eq!(buffer.len(), WINDOW_SIZE);
-  (buffer[0], buffer[1], buffer[2])
-}
-
-impl<'a, I> Iterator for SlidingWindow<'a, I>
+fn buffer_to_sliding_window_item<I>(
+  buffer: &SlidingWindowBuffer<I::Item>,
+) -> SlidingWindowItem<I::Item>
 where
   I: Iterator,
+  I::Item: Clone,
 {
-  type Item = SlidingWindowItem<'a, I::Item>;
+  assert_eq!(buffer.len(), WINDOW_SIZE);
+  (buffer[0].clone(), buffer[1].clone(), buffer[2].clone())
+}
 
-  fn next(&mut self) -> Option<SlidingWindowItem<'a, I::Item>> {
+impl<I> Iterator for SlidingWindow<I>
+where
+  I: Iterator,
+  I::Item: Clone,
+{
+  type Item = SlidingWindowItem<I::Item>;
+
+  fn next(&mut self) -> Option<SlidingWindowItem<I::Item>> {
     match (self.iter.next(), &self.state) {
       (None, SlidingWindowState::IterationDone | SlidingWindowState::IterationNotStarted) => None,
       (None, SlidingWindowState::Iterating) => {
         self.buffer.pop_front();
         self.buffer.push_back(None);
         self.state = SlidingWindowState::IterationDone;
-        Some(buffer_to_sliding_window_item::<I>(&mut self.buffer))
+        Some(buffer_to_sliding_window_item::<I>(&self.buffer))
       }
       // TODO: Implement
       (Some(item), SlidingWindowState::IterationDone) => None,
       // TODO: Implement
       (Some(item), SlidingWindowState::IterationNotStarted) => None,
       // TODO: Implement
-      (Some(item), SlidingWindowState::Iterating) => None,
+      (Some(item), SlidingWindowState::Iterating) => {
+        self.buffer.pop_front();
+        self.buffer.push_back(Some(item));
+        Some(buffer_to_sliding_window_item::<I>(&self.buffer))
+      }
     }
   }
 }
 
-impl<'a, I: Iterator> SlidingWindow<'a, I> {
-  fn new(iter: I) -> SlidingWindow<'a, I> {
+impl<I> SlidingWindow<I>
+where
+  I: Iterator,
+  I::Item: Clone,
+{
+  fn new(iter: I) -> SlidingWindow<I> {
     let buffer = VecDeque::from([None, None, None]);
     SlidingWindow {
       iter: iter,
@@ -72,9 +90,9 @@ fn test_sliding_window() {
   let input = vec![1u32, 2, 3];
   let result: Vec<SlidingWindowItem<u32>> = SlidingWindow::new(input.iter().map(|&x| x)).collect();
   let expected = vec![
-    (None, Some(&1), Some(&2)),
-    (Some(&1), Some(&2), Some(&3)),
-    (Some(&2), Some(&3), None),
+    (None, Some(1), Some(2)),
+    (Some(1), Some(2), Some(3)),
+    (Some(2), Some(3), None),
   ];
 
   assert_eq!(result, expected);
