a9509ed51597fac561a14ebd6aa912efffd42eea
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2022-01-14 11:30:37 -0800

    Sorted out lifetimes and types

--- d18/src/sliding_window.rs
+++ d18/src/sliding_window.rs
@@ -2,14 +2,15 @@ use std::collections::VecDeque;
 
 const WINDOW_SIZE: usize = 3;
 
-type OptionalIteratorItem<I: Iterator> = Option<I::Item>;
-type SlidingWindowItem<'a, I: Iterator> = (
-  &'a OptionalIteratorItem<I>,
-  &'a OptionalIteratorItem<I>,
-  &'a OptionalIteratorItem<I>,
+type OptionalIteratorItem<'a, T> = Option<&'a T>;
+
+type SlidingWindowItem<'a, T> = (
+  OptionalIteratorItem<'a, T>,
+  OptionalIteratorItem<'a, T>,
+  OptionalIteratorItem<'a, T>,
 );
 
-type SlidingWindowBuffer<I: Iterator> = VecDeque<OptionalIteratorItem<I>>;
+type SlidingWindowBuffer<'a, T> = VecDeque<OptionalIteratorItem<'a, T>>;
 
 enum SlidingWindowState {
   BufferToBeFilled,
@@ -17,40 +18,45 @@ enum SlidingWindowState {
   BufferEmptied,
 }
 
-struct SlidingWindow<I: Iterator> {
+struct SlidingWindow<'a, I: Iterator> {
   iter: I,
-  buffer: SlidingWindowBuffer<I>,
+  buffer: SlidingWindowBuffer<'a, I::Item>,
   state: SlidingWindowState,
 }
 
-fn buffer_to_sliding_window_item<I: Iterator>(
-  buffer: SlidingWindowBuffer<I>,
-) -> SlidingWindowItem<I> {
+fn buffer_to_sliding_window_item<'a, 'b, I: Iterator>(
+  buffer: &'a SlidingWindowBuffer<'b, I::Item>,
+) -> SlidingWindowItem<'b, I::Item> {
   assert_eq!(buffer.len(), WINDOW_SIZE);
-  (&buffer[0], &buffer[1], &buffer[2])
+  (buffer[0], buffer[1], buffer[2])
 }
 
-impl<I> Iterator for SlidingWindow<I>
+impl<'a, I> Iterator for SlidingWindow<'a, I>
 where
   I: Iterator,
 {
-  type Item = SlidingWindowItem<I>;
+  type Item = SlidingWindowItem<'a, I::Item>;
 
-  fn next(&mut self) -> Option<SlidingWindowItem<I>> {
-    match (self.iter.next(), self.state) {
-      (None, SlidingWindowState::BufferEmptied) => None,
+  fn next(&mut self) -> Option<SlidingWindowItem<'a, I::Item>> {
+    match (self.iter.next(), &self.state) {
+      (None, SlidingWindowState::BufferEmptied | SlidingWindowState::BufferToBeFilled) => None,
       (None, SlidingWindowState::BufferFilled) => {
         self.buffer.pop_front();
         self.buffer.push_back(None);
-        let item: SlidingWindowItem<I> = buffer_to_sliding_window_item::<I>(self.buffer);
-        Some(item)
+        Some(buffer_to_sliding_window_item::<I>(&mut self.buffer))
       }
+      // TODO: Implement
+      (Some(item), SlidingWindowState::BufferEmptied) => None,
+      // TODO: Implement
+      (Some(item), SlidingWindowState::BufferToBeFilled) => None,
+      // TODO: Implement
+      (Some(item), SlidingWindowState::BufferFilled) => None,
     }
   }
 }
 
-impl<I: Iterator> SlidingWindow<I> {
-  fn new(iter: I) -> SlidingWindow<I> {
+impl<'a, I: Iterator> SlidingWindow<'a, I> {
+  fn new(iter: I) -> SlidingWindow<'a, I> {
     let buffer = VecDeque::from([None, None, None]);
     SlidingWindow {
       iter: iter,
@@ -63,11 +69,11 @@ impl<I: Iterator> SlidingWindow<I> {
 #[test]
 fn test_sliding_window() {
   let input = vec![1u32, 2, 3];
-  let result: Vec<Vec<Option<u32>>> = SlidingWindow::new(input.iter().map(|&x| x)).collect();
+  let result: Vec<SlidingWindowItem<u32>> = SlidingWindow::new(input.iter().map(|&x| x)).collect();
   let expected = vec![
-    [None, Some(1), Some(2)],
-    [Some(1), Some(2), Some(3)],
-    [Some(2), Some(3), None],
+    (None, Some(&1), Some(&2)),
+    (Some(&1), Some(&2), Some(&3)),
+    (Some(&2), Some(&3), None),
   ];
 
   assert_eq!(result, expected);
